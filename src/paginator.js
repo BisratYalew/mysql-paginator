86d49745c4914f04acebed101162456a9a8978fb
bisratyalew <bisratyalew10@gmail.com>
2018-11-25 13:42:15 -0800

    Document the function / function parameters

--- src/paginator.js
+++ src/paginator.js
@@ -1,1 +1,11 @@
-const paginator = async function paginateData(table_name, query, opts) {}
+/**
+ * Paginate Data from Mysql/Maria Databases.  
+ * @name Paginate Data
+ * @function
+ * @param  {String} table_name  Table name to paginate data
+ * @param  {String} per_page    Number of data to get per page
+ * @param  {String} page        Page number to show data
+ */
+const paginator = async function paginateData(table_name, query, opts) {
+    
+}
