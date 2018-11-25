6474d20d50c1eed86a071598dbf427e57519178b
bisratyalew <bisratyalew10@gmail.com>
2018-11-25 13:55:32 -0800

    Added function to prepare pagination values

--- src/paginator.js
+++ src/paginator.js
@@ -23,3 +23,37 @@ const paginator = async function paginateData(table_name, query, opts) {
     knex = await knex(dbConfig);
 
 }
+
+const preparePaginationValues = async (per_page, page, total_docs) => {
+    var total_pages = total_docs/per_page;
+
+    
+    if(per_page > 100) per_page = 100;
+    if(per_page > total_docs) per_page = total_docs;
+  
+    var last_limit = page * per_page;
+    var start_limit = last_limit - per_page;
+  
+    if(Number.isInteger(total_pages) == false) total_pages = Number.parseInt(total_pages) + 1
+      
+    if(Number.isInteger(page) == false) page = Number.parseInt(page) + 1
+       
+    if(Number.isInteger(start_limit) == false) {
+       start_limit = Number.parseInt(start_limit)  
+    }
+  
+    if(start_limit < 0) start_limit = 0    
+  
+    if(page==0) page=1;      
+   
+    pagination_data = {
+       total_pages: total_pages,
+       total_docs: total_docs,
+       per_page: per_page,
+       page: page-1 || page,
+       start_limit: start_limit,
+       last_limit: last_limit
+    }
+  
+    return pagination_data;
+}
