94d8fee63e88e214217d834f3891a0df79d37b8f
bisratyalew <bisratyalew10@gmail.com>
2018-11-25 13:54:00 -0800

    Pass db configurations to knex

--- src/paginator.js
+++ src/paginator.js
@@ -8,11 +8,18 @@ const knex      = require('knex');
  * @param  {String} per_page    Number of data to get per page
  * @param  {String} page        Page number to show data
  */
-const paginator = async function paginateData(table_name, query, opts) {  
+const paginator = async function paginateData(table_name, query, opts) { 
+    
+    let dbConfig = {
+        connection: this.dbConfig
+    }
     
     if(!query) throw new Error('You must have to pass query');
 
     if(!this.dbConfig.client) dbConfig.client = 'mysql';
 
     if(!table_name) throw new Error('You must have to specify table name');
+
+    knex = await knex(dbConfig);
+
 }
