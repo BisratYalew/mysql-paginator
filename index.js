94fc45417bdc4eeb9ad9bae8bec91fa97405a02c
bisratyalew <bisratyalew10@gmail.com>
2018-11-25 13:32:02 -0800

    Added database configuration parameter on the constructor

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 class Paginator {
-    constructor(dbConfig= {}){
 
+    constructor(dbConfig= {}){
+        if(!dbConfig || !dbConfig.database || !dbConfig.host ||
+           !dbConfig.user || !dbConfig.password) throw new Error("Database Configurations are needed!");
+        else {
+            this.dbConfig = _.pick(dbConfig, 'host', 'database', 'user', 'client', 'config', 'password');
+        }
     };
     
     /**
      * Paginate
      */
     paginate () {
-        
+
     }
     
 }
